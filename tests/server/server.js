91292305541c4f1dbb4ba5e39bc5e187ef1699ca
Phoscur <phoscur@pheelgood.net>
2020-01-02 22:24:21 +0100

    Add npm start command starting vue cli and mock api in parallel

--- tests/server/server.js
+++ tests/server/server.js
@@ -1,48 +1,46 @@
 
-const expressBodyParser = require('body-parser');
+const expressBodyParser = require("body-parser");
 
-const packageInfo = require('./package.json');
+const packageInfo = require("./package.json");
 // JSON Server mock
-const { server, router, middlewares, rules } = require('./mock-json-server');
+const { server, router, middlewares, rules } = require("./mock-json-server");
 
-server.set('svcInfo', packageInfo);
-server.set('port', process.env.API_PORT || 3200);
+server.set("svcInfo", packageInfo);
+server.set("port", process.env.API_PORT || 3200);
 
-
-server.use(expressBodyParser.urlencoded({limit: '10mb', extended: true}));
-server.use(expressBodyParser.json({limit: '10mb'}));
+server.use(expressBodyParser.urlencoded({ limit: "10mb", extended: true }));
+server.use(expressBodyParser.json({ limit: "10mb" }));
 
 server.use(middlewares);
 server.use(rules);
 
-server.get("/v1/users/test", (req, res) => res.jsonp({email: "test@test.com", username: "test"}));
+server.get("/test/users/test", (req, res) => res.json({ email: "test@test.com", username: "test" }));
 
 // test route that always returns the request body with 200
-server.post('/reflect200', (req, res) => res.jsonp(req.body));
+server.post("/reflect200", (req, res) => res.jsonp(req.body));
 
-server.get('/info', (req, res) => {
-  res.json({name: server.get('svcInfo').name, version: server.get('svcInfo').version});
+server.get("/info", (req, res) => {
+  res.json({ name: server.get("svcInfo").name, version: server.get("svcInfo").version });
 });
 
 server.use(router);
 
 // Start server
-server.listen(server.get('port'), function () {
-    console.log('Express server listening on port ' + server.get('port'));
-    console.log('Service url: http://localhost:' + server.get('port'));
-    console.log('Service name: ' + server.get('svcInfo').name + ', version: ' + server.get('svcInfo').version);
+server.listen(server.get("port"), function() {
+  console.log("Service name: " + server.get("svcInfo").name + ", version: " + server.get("svcInfo").version);
+  console.log("Service url: http://localhost:" + server.get("port"));
 });
 
 
-process.on('SIGTERM', function () {
-  console.log('SIGTERM: Exiting ...');
+process.on("SIGTERM", function() {
+  console.log("SIGTERM: Exiting ...");
   process.exit(0)
 });
 
-process.on('SIGINT', function () {
+process.on("SIGINT", function() {
   process.exit(-1)
 });
 
-process.on('uncaughtException', function (err) {
+process.on("uncaughtException", function(err) {
   process.exit(-1);
 });
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
-const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin');
+const VuetifyLoaderPlugin = require("vuetify-loader/lib/plugin");
+const webpack = require("webpack")
 
 const release = require("./package.json").version;
 // Export app version (VUE_APP_* env exports only)
@@ -8,41 +9,33 @@ const port = process.env.PORT || 8080;
 const public = process.env.HTTP_HOST || undefined;
 const publicPath = process.env.PUBLIC_URL || "./";
 const host = "0.0.0.0";
-const proxy = 'http://0.0.0.0:'+(process.env.API_PORT || 3200);
+const proxy = "http://0.0.0.0:" + (process.env.API_PORT || 3200);
+const production = process.env.production || process.env.NODE_ENV === "production";
 
 console.log(`Vue CLI building UGamela Client in version: ${release}`);
 console.log(`Host: ${public || "localhost"}:${port} - publicPath: "${publicPath}"`);
 
+const plugins = [
+  new VuetifyLoaderPlugin(), // for custom dynamic imports see https://vuetifyjs.com/en/customization/a-la-carte#custom-dynamic-imports
+];
+
+if (!production) {
+  plugins.push(new webpack.HotModuleReplacementPlugin());
+  console.log("Proxy through to mock API at", proxy);
+}
+
 module.exports = {
   configureWebpack: {
-    plugins: [
-      new VuetifyLoaderPlugin(), // for custom dynamic imports see https://vuetifyjs.com/en/customization/a-la-carte#custom-dynamic-imports
-    ],
+    plugins,
   },
   publicPath,
   devServer: {
     proxy,
-    disableHostCheck: true, // behind nginx proxy this is allowed
     host,
-    hot: true,
-    // inline: false,
+    hot: !production,
     public,
     port,
     overlay: true,
-    /*
-    // Access & extend builtin express server:
-    before(app) {
-       app.get("/some-route", (req, res) => {
-        res.set("Content-Type", "text/html");
-        const { code } = req.query;
-        res.send(`
-          <body onload="window.top.postMessage('success-code=${code}', '${publicPath}');">
-          Success
-          </body>
-        `);
-      });
-    },
-    */
   },
   chainWebpack: config => {
     /*config.module
